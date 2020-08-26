9649979d781fc84513dd39e4758514f015644cdb
Nick Sotula <so2niko@gmail.com>
2020-08-26 11:03:32 +0300

    docs: loops

---
+++ les03/app/main.js
@@ -0,0 +1,25 @@
+let i = 0;
+
+// while(i < 10){
+//     console.log('i: ', i);
+//     i++;
+// }
+
+// for(let k = 0; k < 10; k++){
+//     console.log('k: ', k);
+// }
+
+for(let kk = 10, name = '', arr = [], oo = {}; kk > 0 && kk < 1000 || kk == -1020 ;){
+    console.log(kk);
+}
+
+
+
+let j = 110;
+
+// do{
+//     console.log('j: ', j);
+//     j++;
+// }while(j < 10);
+
+
